ce26b13bfaf26b7ba20f65b33f6892804db8fdb8
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-04-25 15:54:30 +0545

    Rename file

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
-const companyParse = require('./companyParse.mjs');
+const companyScraper = require('./companyScraper.mjs');
 
 if (require.main === module) {
   const config = JSON.parse(fs.readFileSync('./config.json'));
-  companyParse(config['COMPANY_SYMBOL']);
+  companyScraper(config['COMPANY_SYMBOL']);
 }
