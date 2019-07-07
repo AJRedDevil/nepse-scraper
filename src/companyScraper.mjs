51c95aedde8a638196b9814576fdf634f1b21613
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-07-07 08:43:10 +0545

    Add Company ID and extend the limit to 1000

--- src/companyScraper.mjs
+++ src/companyScraper.mjs
@@ -8,14 +8,25 @@ const fs = require('fs');
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 
+const options = {
+  _limit: 1000,
+};
+
 const parseHtml = selector => $ => $(selector);
 const findHtml = selector => html => cheerio(html).find(selector);
-const parseText = item => cheerio(item).text().trim();
+const parseText = item =>
+  cheerio(item)
+    .text()
+    .trim();
+const parseHref = item =>
+  cheerio(item)
+    .find('a')
+    .attr('href')
+    .split('/')
+    .reverse()[0];
 const parseRows = allItems => {
-  const filteredHeaderFooters = allItems.slice(2, allItems.length - 1)
-  return filteredHeaderFooters
-    .map((i, item) => findHtml('td')(item))
-    .get();
+  const filteredHeaderFooters = allItems.slice(2, allItems.length - 1);
+  return filteredHeaderFooters.map((i, item) => findHtml('td')(item)).get();
 };
 const companyListing = items => {
   const companies = [];
@@ -24,30 +35,30 @@ const companyListing = items => {
       name: parseText(rows[2]),
       symbol: parseText(rows[3]),
       sector: parseText(rows[4]),
-    })
+      companyId: parseHref(rows[5]),
+    });
   }
   return companies;
-}
+};
 const printSymbol = items => {
   for (let item of items) {
-    console.log(item)
+    console.log(item);
   }
-}
+};
 const writeSymbol = items => {
-  const stats = fs.statSync('data');
-  if (!stats.isDirectory()) {
+  if (!fs.existsSync('data')) {
     fs.mkdirSync('data');
   }
-  fs.writeFileSync('data/symbol.json', JSON.stringify(items), 'utf-8');
-}
+  fs.writeFileSync('data/symbol.json', JSON.stringify(items, null, 2), 'utf-8');
+};
 
-
-companySymbolScraper = (url) => rp(url)
-  .then(cheerio.load)
-  .then(parseHtml('#company-list tr'))
-  .then(parseRows)
-  .then(companyListing)
-  .then(writeSymbol)
-  .catch(console.error)
+companySymbolScraper = url =>
+  rp(url, {qs: options})
+    .then(cheerio.load)
+    .then(parseHtml('#company-list tr'))
+    .then(parseRows)
+    .then(companyListing)
+    .then(writeSymbol)
+    .catch(console.error);
 
 module.exports = companySymbolScraper;
